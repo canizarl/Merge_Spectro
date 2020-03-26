17b75adc2a6fee6a283e61ef890ca86bb556021c
Alberto Cañizares <albertocanizares@Albertos-MacBook-Pro-2.local>
2020-03-26 11:43:55 +0000

    making add gaps

--- psp_dataprep.py
+++ psp_dataprep.py
@@ -44,6 +44,24 @@ import os
 
 
 class fnames:
+    """
+    This class stores the location of the directories where data is, as well as the names of the datasets to be extracted
+    from CDF file. 
+
+    
+    band: Either hfr or lfr for high band or low band
+    year: year of the observation
+    month: month of the observation
+    day: day of the observation 
+    dipoles: Antenna pair V1V2 or V3V4
+
+    fname - file name
+    path_data - path to the data (for now change here but in the future this will be an option)
+    dataname - name of dataset in the CDF file
+    epochname - name of epoch dataset in the CDF file
+    freqname - name of frequency dataset in the CDF file
+
+    """
     def __init__(fnames,band,year,month,day,dipoles):
         if dipoles == "V1V2":
             ch = "0"
@@ -66,10 +84,35 @@ class fnames:
         fnames.freqname = "frequency_"+fnames.band+"_auto_averages_ch"+ch+"_"+fnames.dipoles
 
 
+class data_spectro:
+    """
+    Data class. Data extracted from CDF file will be stored in this class format. 
+    With:
+    data : 2D matrix of the dynamic spectra
+    epoch: 1D array of the time datapoints
+    freq: 1D array of the frequency channels used for each data point. 
+    """
+    def __init__(data_spectro, data, epoch, freq):
+        data_spectro.data=data
+        data_spectro.epoch=epoch
+        data_spectro.freq=freq
 
 
 
 def data_from_CDF(date, myfile):
+    """ data_from_CDF
+     outputs dynamic spectra data from PSP CDF datafile. 
+     inputs:
+        date: datetime.datetime class. Date of the observation. 
+        myfile: fnames class defined in psp_dataprep. Location of all directories 
+
+    output:
+        data: 2D numpy matrix of dynamic spectra
+        epoch: 1D numpy array of the date and times of each datapoint
+        freqs: 1D numpy array of the frequency channels 
+
+    """
+
     cwd = os.getcwd()
     cdf = pycdf.CDF(myfile.path_data)
     # print(cdf)
@@ -91,6 +134,71 @@ def data_from_CDF(date, myfile):
     return data, epoch, freqs    
 
 
+def add_gaps(data):
+
+    # epochtest = np.subtract(epochl,epochh)
+    # for i in range(len(epochtest)):
+    #     print(epochtest[i])
+
+
+    # time resolution
+    timediff = []
+    for i in range(0,len(data.epoch)-1):
+        diff = data.epoch[i+1]-data.epoch[i]
+        #print(diff)
+        buffer = diff.total_seconds()
+        timediff.append(float(buffer))
+
+    timeres = np.mean(timediff)
+    print(f"Time res: {timeres}")
+    cadence=mode(timediff)
+    thres = cadence*2
+    thres=float(thres)
+
+    inseconds = []
+    for each in timediff:
+        inseconds.append(float (abs(each)))
+
+    if developer == 1:
+        if verbose == 1:
+            print(inseconds)
+        print(f"inseconds type {type(inseconds[1])}")
+        print(f"thres type {type(thres)}")
+        print(f"timediff type {type(timediff[i])}")
+
+    
+        
+    gap_indices = []
+    for i in range(0,len(inseconds)):
+        if inseconds[i]>thres:
+            gap_indices.append(i)
+
+    if developer == 1:
+        print(f"Length gap_indices: {len(gap_indices)}")
+        print(f"gap_indices: {gap_indices}")
+
+
+    if developer == 1:
+        plt.figure(2)
+        plt.plot(inseconds[:], 'r-')
+        plt.plot(inseconds[:], 'r*')
+        c = 0
+        for i in range(0, len(inseconds)):
+            if c < len(gap_indices):
+                if i == gap_indices[c]:
+                    plt.plot(gap_indices[c],inseconds[i],'b*')    
+                    c= c+1
+            else:
+                break
+        plt.show()
+
+    
+    return data
+            
+
+
+
+
 
 
 
@@ -99,6 +207,8 @@ def data_from_CDF(date, myfile):
 
 
 if __name__=='__main__':
+    developer = 1
+    verbose = 0
     starttime = dt.datetime.now()
     print(' ')
     print(' ')
@@ -107,7 +217,9 @@ if __name__=='__main__':
 
     year = "2019"
     month = "04"
-    day = "09"
+    day = "18"
+
+    add_gaps_option= 1
 
 
     t0 = dt.datetime(int(year),int(month),int(day))
@@ -142,33 +254,35 @@ if __name__=='__main__':
     freql = freqsl12                    # same as in V3V4
     epochl = epochl12                   # same as in V3V4
 
-
-
-
-    # epochtest = np.subtract(epochl,epochh)
-    # for i in range(len(epochtest)):
-    #     print(epochtest[i])
-
-
-    # time resolution
-    timediff = []
-    for i in range(0,len(epochh)-1):
-        diff = epochh[i+1]-epochh[i]
-        #print(diff)
-        timediff.append(diff)
-
-    timeres = np.mean(timediff)
-    print(f"Time res: {timeres}")
-
-
-
-    # add gaps   
-
+    lsd = data_spectro(datal,epochl,freql)      #lowband spectral data
+    hsd = data_spectro(datah,epochh,freqh)      #highband spectral data
 
     
 
+    # add gaps 
+    if add_gaps_option == 1:
+        print("add_gaps not implemented yet")
+        add_gaps(lsd)
+        #add_gaps(hsd)
 
     
+    
+
+
+
+    
+    
+    
+    
+    if developer == 1:
+        # quick plot for now. 
+        v_min = np.percentile(hsd.data, 1)
+        v_max = np.percentile(hsd.data, 99)
+        plt.figure(1)
+        plt.imshow(hsd.data.T, aspect='auto', vmin=v_min, vmax=v_max)
+        plt.show()
+        
+
 
 
 

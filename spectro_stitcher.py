31c2f140c13103b5076b5fc868bce5b1080bc9cb
Alberto Cañizares <albertocanizares@Albertos-MacBook-Pro-2.local>
2020-04-02 18:06:14 +0100

    spectro stitcher created

---
+++ spectro_stitcher.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+from datetime import timedelta
+from psp_dataprep import data_spectro
+
+
+
+## import data
+#import PSP h data
+
+
+#import PSP l data
+
+
+#import lofar data
+
+
+
+#plots
+
+
+
+
+
+
+
+
